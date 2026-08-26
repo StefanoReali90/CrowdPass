88360ee0549f96d55ac4c75c86de836f6fdda9f0
Reptile90 <stefano.reali1990@gmail.com>
2026-08-27 00:54:52 +0200

    Update gitignore, User entity e application properties

--- src/main/java/org/spring/crowdpass/user/entity/User.java
+++ src/main/java/org/spring/crowdpass/user/entity/User.java
@@ -1,4 +1,28 @@
 package org.spring.crowdpass.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String cognome;
+
+
+
+
 }
