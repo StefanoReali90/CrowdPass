3079c6aa1c4c510491ad0a4d0cd9deefe756e6b7
Reptile90 <stefano.reali1990@gmail.com>
2026-08-27 00:51:37 +0200

    first commit

---
+++ src/main/java/org/spring/crowdpass/CrowdPassApplication.java
@@ -0,0 +1,13 @@
+package org.spring.crowdpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdPassApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/spring/crowdpass/user/entity/User.java
@@ -0,0 +1,4 @@
+package org.spring.crowdpass.user.entity;
+
+public class User {
+}
